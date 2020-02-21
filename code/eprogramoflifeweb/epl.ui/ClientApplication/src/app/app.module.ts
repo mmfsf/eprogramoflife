8977bc801381106fc6c6bf20d3ddffd5d48cd0aa
Marcos Farias <Marcos_Farias@epam.com>
2020-02-21 12:34:14 +0100

    update angular material

--- code/eprogramoflifeweb/epl.ui/ClientApplication/src/app/app.module.ts
+++ code/eprogramoflifeweb/epl.ui/ClientApplication/src/app/app.module.ts
@@ -6,19 +6,17 @@ import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {
-  MatButtonModule,
-  MatCheckboxModule,
-  MatToolbarModule,
-  MatListModule,
-  MatIconModule,
-  MatGridListModule,
-  MatFormFieldModule,
-  MatCardModule,
-  MatProgressSpinnerModule,
-  MatInputModule,
-  MatMenuModule
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
